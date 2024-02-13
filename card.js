a5c88b2359a1324bda1d45dd5b4eeb94f47cc6a5
Sameer Sheikh <109454092+sameersk11@users.noreply.github.com>
2024-02-14 02:14:56 +0530

    Add files via upload
    first commit

---
+++ card.js
@@ -0,0 +1,21 @@
+// For Valentine Card
+
+$(document).ready(function () {
+    $(".container")
+      .mouseenter(function () {
+        $(".card").stop().animate(
+          {
+            top: "-90px",
+          },
+          "slow"
+        );
+      })
+      .mouseleave(function () {
+        $(".card").stop().animate(
+          {
+            top: 0,
+          },
+          "slow"
+        );
+      });
+  });
